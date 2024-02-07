e8193afcd44c5e75d95e4c6cf9780dfaa48eee9e
Susan Zhou <susan.0.zhou@gmail.com>
2024-02-06 21:32:20 -0800

    applying @CrossOrigin annotation to ReviewController and AnimeController class

--- src/main/java/dev/susan/animes/controller/AnimeController.java
+++ src/main/java/dev/susan/animes/controller/AnimeController.java
@@ -2,18 +2,14 @@ package dev.susan.animes.controller;
 
 import dev.susan.animes.entity.Anime;
 import dev.susan.animes.service.AnimeService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/animes")
 public class AnimeController {
